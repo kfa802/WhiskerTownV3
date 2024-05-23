365a58169776ccb2be4fd4f268fd5da004a49736
kfa802 <147632358+kfa802@users.noreply.github.com>
2024-05-23 13:34:25 +0200

    Structure

--- Assets/Scripts/Sky.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Sky : MonoBehaviour
-{
-  //code sky object that moves along the x axis to the left
-
-}
