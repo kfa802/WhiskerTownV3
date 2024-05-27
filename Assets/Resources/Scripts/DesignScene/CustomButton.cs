044329b4b16466e2607155c11d04a4eb5fef2349
EmilieSommer <emilie.freltoft@gmail.com>
2024-05-27 08:53:40 +0200

    Comments added

--- Assets/Resources/Scripts/DesignScene/CustomButton.cs
+++ Assets/Resources/Scripts/DesignScene/CustomButton.cs
@@ -1,35 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.UI; //This was added to manipulate the UI elements, like buttons. But not really used anymore, but I dind't see a reason to delete. Copilot was used to suggest this.
 
 public class CustomButton : MonoBehaviour
 {
     [System.Serializable]
+    // This class is used to store the game objects (the cat itself) for each cat and their clothes (list of clothes)
     public class Cat
     {
         public GameObject catObject;
         public List<GameObject> clothes;
     }
-
+    //This is the cats :)
     public Cat pusling;
     public Cat musling;
     public Cat misling;
 
+    //These are the buttons :)
     public UnityEngine.UI.Button puslingButton;
     public UnityEngine.UI.Button muslingButton;
     public UnityEngine.UI.Button mislingButton;
 
+    //This is the Wanted poster (a UI image, with button functionality(sends you to the main screen via scenemanager)) activated when the caemra button is pressed.
     public GameObject wantedObject;
 
     void Start()
     {
-        // Deactivate all cats and their clothes at the start (and wanted object)
+        // Deactivate the wanted object and calls the DeactivateAllCats method
         wantedObject.SetActive(false);
         DeactivateAllCats();
         
     }
 
+    //This method deactivates all cats (calls the DeactivateCat method below))
     void DeactivateAllCats()
     {
         DeactivateCat(pusling);
@@ -37,6 +41,7 @@ public class CustomButton : MonoBehaviour
         DeactivateCat(misling);
     }
 
+    //This method checks and deactivates the single cat and its clothes, if they exist (not null) we don't want two cats to be active at the same time.
     void DeactivateCat(Cat cat)
     {
         if (cat.catObject != null)
@@ -51,7 +56,9 @@ public class CustomButton : MonoBehaviour
             }
         }
     }
+    
 
+    //This method activates the cat and its clothes, this method is connected to the buttons in the inspector.
     void ActivateCat(Cat cat)
     {
         // Deactivate all cats first
@@ -71,6 +78,7 @@ public class CustomButton : MonoBehaviour
         }
     }
 
+    //These methods are connected to the buttons in the inspector. Copilot was used generate these buttons.
     public void ActivatePusling()
     {
         ActivateCat(pusling);
--- Assets/Resources/Scripts/DesignScene/DragandDrop.cs
+++ Assets/Resources/Scripts/DesignScene/DragandDrop.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class DragandDrop : MonoBehaviour
 {
+   //This method is called when the mouse is clicked and dragged, it's a super easy unity function 
      void OnMouseDrag()
  {
     Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
     transform.position = mousePos;
  }
 }
+// I found this code in the first comment by @topBagon under this youtube video: https://www.youtube.com/watch?v=We1ab6yHrwI&list=PLBIb_auVtBwCK_dyk-Wzxvk5I5z441lZd&index=6
